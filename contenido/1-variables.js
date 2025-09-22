db6ddb7c98d8ea635dbf35922a1e0486baa2b16c
BradOlv <boliva-2023502@kinal.edu.gt>
2025-09-22 11:56:30 -0600

    002 - agregamos elementos básicos de javascript

---
+++ contenido/1-variables.js
@@ -0,0 +1,19 @@
+//buenas prácticas ser descriptivo
+let cajaAudifonos = "derecho"
+let urlDeUsuario = "https://www.google.com"
+let carnetAlumno = "2023502"
+
+//mala práctica, no usar variables ambiguas 
+let c = "derecho"
+let ca = "derecho"
+let caUsuario = "derecho"
+
+console.log(cajaAudifonos)
+
+//otras formas
+
+var nombre = "Bradley"
+nombre = "Carlos"
+var edad; 
+
+const PI = 3.14159
---
+++ contenido/3-String.js
@@ -0,0 +1,16 @@
+// tipos de datos string 
+
+let string1 = "Hola, mundo"
+let string2 = "JavaScript es facilito"
+let string3 = `${string1} contento`
+let string4 = string1 + "" + string2
+
+//console.log(string1)
+//console.log(string2)
+//console.log(string3)
+
+let frase = "M37: Aprendo a rezar el rosario"
+console.log(frase.length)
+console.log(frase.toLocaleLowerCase())
+console.log(frase.toUpperCase())
+console.log(frase.substring(0, 19))
