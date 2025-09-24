ac173e6635924551032a9682652883ea3455183e
BradOlv <boliva-2023502@kinal.edu.gt>
2025-09-23 21:15:45 -0600

    E12-suma1al50 terminado

---
+++ lab-uno/E12-suma1al50.js
@@ -0,0 +1,10 @@
+// Ejercicio 12: Suma de todos los números del 1 al 50 usando un bucle for
+
+
+function ejercicio12() {
+    let suma = 0;
+    for (let i = 1; i <= 50; i++) {
+        suma += i;
+    }
+    alert("La suma de los números del 1 al 50 es: " + suma);
+}
